f22861bdf50fb7430e65830b46f92e36a8b1dfa8
jackyuan <jackyuan@jackyuandeMacBook-Pro.local>
2014-12-18 22:36:19 +0800

    4324

---
+++ myswift3/myswift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myswift3
+//
+//  Created by jackyuan on 14/12/18.
+//  Copyright (c) 2014年 jackyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
